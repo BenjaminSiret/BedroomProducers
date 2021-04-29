3fc6e4bf9bfe3d020f3072c4484e18831340fa27
Benjamin Siret <benjaminsiret@outlook.fr>
2021-04-29 16:46:50 +0200

    create comments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_144043) do
+ActiveRecord::Schema.define(version: 2021_04_29_144539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.text "undercomment"
+    t.bigint "track_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["track_id"], name: "index_comments_on_track_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "tracks", force: :cascade do |t|
     t.string "title"
@@ -50,6 +61,8 @@ ActiveRecord::Schema.define(version: 2021_04_29_144043) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "tracks"
+  add_foreign_key "comments", "users"
   add_foreign_key "tracks", "users"
   add_foreign_key "upvotes", "tracks"
   add_foreign_key "upvotes", "users"
