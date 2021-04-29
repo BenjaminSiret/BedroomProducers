0c9122599142a499e170f26f2ddda8bc92b9c751
Theo Agriodos <theo.agriodos@gmail.com>
2021-04-29 17:51:02 +0200

    validations on models

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,14 @@ class User < ApplicationRecord
   has_many :messages
   has_one_attached :photo
 
+  validates :username, presence: true, uniqueness: true
+  validates :email, format: { with: /\A.*@.*\.com\z/ },prensece: true ,uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :status, presence: true
+  validates :description, presence: true
+  validates :avatar, presence: true
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
