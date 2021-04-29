52722cd8dd12dd9060ea3a318d02a01ea5885905
Theo Agriodos <theo.agriodos@gmail.com>
2021-04-29 17:15:27 +0200

    belongs and has many added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :tracks
+  has_many :upvotes
+  has_many :messages
+  has_one_attached :photo
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
