c5f181c6fd322009c8b4696fa256a7c8edea16da
Benjamin Siret <benjaminsiret@outlook.fr>
2021-05-09 18:21:25 +0200

    create messages model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_161454) do
+ActiveRecord::Schema.define(version: 2021_05_09_161803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,16 @@ ActiveRecord::Schema.define(version: 2021_05_09_161454) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "tracks", force: :cascade do |t|
     t.string "title"
     t.string "url"
@@ -99,6 +109,8 @@ ActiveRecord::Schema.define(version: 2021_05_09_161454) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "tracks"
   add_foreign_key "comments", "users"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "tracks", "users"
   add_foreign_key "upvotes", "tracks"
   add_foreign_key "upvotes", "users"
