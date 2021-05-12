3c10ef817ebe6edcea6cb2d38292792926e87cd0
morgane duluc <morgane.duluc@gmail.com>
2021-05-12 17:39:43 +0200

    show_track

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :chatrooms, only: :show do
     resources :messages, only: :create
-  resources :tracks
   end
+  resources :tracks, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
