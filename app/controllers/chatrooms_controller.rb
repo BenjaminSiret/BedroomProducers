f826e26cca608371b12e02856536a6b133901940
Benjamin Siret <benjaminsiret@outlook.fr>
2021-05-14 15:04:23 +0200

    add actions in chatrooms_controller, add a partial in chatroom view

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,30 @@
 class ChatroomsController < ApplicationController
+  def index
+    @chatrooms = Chatroom.all
+  end
   def show
     @chatroom = Chatroom.find(params[:id])
     @user = current_user
     @message = Message.new
+  end
+
+  def new
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+    if @chatroom.save
+      flash[:success] = "Chatroom #{@chatroom.name} was created successfully"
+      redirect_to chatroom_path(@chatroom)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :chatrooms, only: :show do
+  resources :chatrooms, only: [:index, :show, :create, :new] do
     resources :messages, only: :create
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
